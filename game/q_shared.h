91c5fa3d9e12635306ecec705c5e6fdf0e32fa74
Bill Currie <bill@taniwha.org>
2001-12-22 05:37:39 +0000

    almost links

--- game/q_shared.h
+++ game/q_shared.h
@@ -39,11 +39,11 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <stdlib.h>
 #include <time.h>
 
-#if (defined _M_IX86 || defined __i386__) && !defined C_ONLY && !defined __sun__
-#define id386	1
-#else
+//#if (defined _M_IX86 || defined __i386__) && !defined C_ONLY && !defined __sun__
+//#define id386	1
+//#else
 #define id386	0
-#endif
+//#endif
 
 #if defined _M_ALPHA && !defined C_ONLY
 #define idaxp	1
