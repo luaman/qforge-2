5d2053580dcd9379d711d45c89a886343d263584
Jamie Wilkinson <jaq@spacepants.org>
2002-05-19 17:13:44 +0000

    - Added __FreeBSD__ to some locations so that things build   properly

--- src/gl_rmain.c
+++ src/gl_rmain.c
@@ -1317,7 +1317,7 @@ qboolean R_Init( void *hinstance, void *hWnd )
 		ri.Con_Printf( PRINT_ALL, "...GL_EXT_point_parameters not found\n" );
 	}
 
-#ifdef __linux__
+#if defined __linux__ || defined __FreeBSD__
 	if ( strstr( gl_config.extensions_string, "3DFX_set_global_palette" ))
 	{
 		if ( gl_ext_palettedtexture->value )
